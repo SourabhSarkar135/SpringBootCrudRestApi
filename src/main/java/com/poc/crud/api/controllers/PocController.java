504832c65c8478f529cce44dd6d640f157c52107
Sourabh Sarkar <sourabh.sarkar@ah.nl>
2021-09-06 17:16:18 +0530

    Refactored!

--- src/main/java/com/poc/crud/api/controllers/PocController.java
+++ src/main/java/com/poc/crud/api/controllers/PocController.java
@@ -13,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -68,7 +67,6 @@ public class PocController {
 	@PostMapping(value=UriConstants.CREATE_RECORD)
 	public ResponseEntity<PocBean> createRecord(
 			@RequestBody PocBean pocBean) {
-		System.out.println("Hi");
 		try {
 			pocServices.save(pocBean);
 			return new ResponseEntity<>(HttpStatus.CREATED);
