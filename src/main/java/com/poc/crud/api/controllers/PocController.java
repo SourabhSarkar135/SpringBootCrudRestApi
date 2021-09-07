33a980d2effaaf74f192cb2ae36a379820e1c081
Sourabh Sarkar <sourabh.sarkar@ah.nl>
2021-09-07 18:33:29 +0530

    Implemented CORS

--- src/main/java/com/poc/crud/api/controllers/PocController.java
+++ src/main/java/com/poc/crud/api/controllers/PocController.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.poc.crud.api.beans.PocBean;
 import com.poc.crud.api.constants.UriConstants;
 import com.poc.crud.api.services.PocServices;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping(value = UriConstants.API_VERSION_1)
 public class PocController {
